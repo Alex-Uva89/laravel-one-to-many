db0e1c5d1135f8464c502d4bd51b5b5200b621c0
ALESSANDROUVA <a.uva1988@gmail.com>
2022-05-24 17:48:31 +0200

    controllo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,17 +13,17 @@
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
-    <body class="bg-secondary">
+    <body class="bg-magic">
         @include('partials.headeradmin')
 
         <main class="py-4">
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-12 col-md-2 pt-5 ps-5">
-                        <div class="py-2"><a href="{{ route('admin.posts.index') }}" class="btn btn-info float-right">Tutti i post</a></div>
-                        <div class="py-2"><a href="{{ route('admin.posts.create') }}" class="btn btn-info float-right">Crea nuovo post</a></div>
-                        <div class="py-2"><a href="{{ route('admin.categories.index') }}" class="btn btn-info float-right">Tutte le categorie</a></div>
-                        <div class="py-2"><a href="{{ route('admin.categories.create') }}" class="btn btn-info float-right">Crea nuova categoria</a></div>
+                        <div class="py-2"><a href="{{ route('admin.posts.index') }}" class="btn btn-outline-info bg-white float-right">Tutti i post</a></div>
+                        <div class="py-2"><a href="{{ route('admin.posts.create') }}" class="btn btn-outline-info bg-white float-right">Crea nuovo post</a></div>
+                        <div class="py-2"><a href="{{ route('admin.categories.index') }}" class="btn btn-outline-info bg-white float-right">Tutte le categorie</a></div>
+                        <div class="py-2"><a href="{{ route('admin.categories.create') }}" class="btn btn-outline-info bg-white float-right">Crea nuova categoria</a></div>
                     </div>
                     <div class="col-12 col-md-10">
                         @yield('pageContent')
